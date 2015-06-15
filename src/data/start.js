081a110624ee74da4628622493a120689e2b2c50
InBasic <inb.cor@gmail.com>
2015-06-15 10:20:57 +0430

    bug #31 follow up

--- src/data/start.js
+++ src/data/start.js
@@ -13,10 +13,13 @@ if (!self.options.prefs.sidebar && $('watch7-sidebar')) {
 if (!self.options.prefs.discussion && $('watch-discussion')) {
   $('watch-discussion').style.display = 'none';
 }
-if (!self.options.prefs.views && $('watch7-views-info')) {
-  $('watch7-views-info').style.display = 'none';
+if (!self.options.prefs.views && $('watch8-sentiment-actions')) {
+  $('watch8-sentiment-actions').style.display = 'none';
 }
-if (!self.options.prefs.actions && $('watch8-action-buttons')) {
+if (!self.options.prefs.actions && $('watch8-secondary-actions')) {
+  $('watch8-secondary-actions').style.display = 'none';
+}
+if (!self.options.prefs.views && !self.options.prefs.actions && $('watch8-action-buttons')) {
   $('watch8-action-buttons').style.display = 'none';
 }
 if (!self.options.prefs.details && $('action-panel-details')) {
